f17a181d3d62c9ff8f9365eb0069f70295b5199a
storezhang <storezhang@gmail.com>
2025-04-27 12:47:55 +0800

    refactor(项目): 使用依赖注入重构

--- internal/config/id.go
+++ internal/config/id.go
@@ -9,7 +9,7 @@ type Id struct {
 	Snowflake *Snowflake `json:"snowflake,omitempty" validate:"required"`
 }
 
-func newId(config *config.Getter) (id *Id, err error) {
+func newId(config config.Getter) (id *Id, err error) {
 	id = new(Id)
 	err = config.Get(&struct {
 		Id *Id `json:"id,omitempty" validate:"required"`
